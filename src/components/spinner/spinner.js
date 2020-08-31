861c7061da81b22f7161ea2adb21aa61772e8289
Roman <devilog@rambler.ru>
2020-08-31 17:07:44 +0300

    random character block

---
+++ src/components/spinner/spinner.js
@@ -0,0 +1,15 @@
+import React from 'react'; 
+import './spinner.css';
+
+const Spinner = () => {
+    return (
+        <div class="loadingio-spinner-double-ring-cd3wlp60v"><div class="ldio-v364ej6is5">
+            <div></div>
+            <div></div>
+            <div><div></div></div>
+            <div><div></div></div>
+        </div></div>
+    )
+}
+
+export default Spinner;
--- src/services/gotService.js
+++ src/services/gotService.js
@@ -1,4 +1,4 @@
-class GotService {
+export default class GotService {
 
     constructor() {
         this._apiBase = 'https://www.anapioficeandfire.com/api';
@@ -12,21 +12,65 @@ class GotService {
         }
 
         return await res.json();
-    };
-    getAllCharacters() {
-        return this.getResource('/characters?page=5&pageSize=10');
     }
-    getCharacter(id) {
-        return this.getResource(`/characters/${id}`);
+
+    async getAllCharacters() {
+        const res = await this.getResource('/characters?page=5&pageSize=10');
+        return res.map(this._transformCharacter);
+    }
+    
+    async getCharacter(id) {
+        const character = await this.getResource(`/characters/${id}`);
+        return this._transformCharacter(character);
+    }
+
+    async getAllHouses() {
+        const res = await this.getResource(`/houses/`);
+        return res.map(this._transformHouse);
+    }
+
+    async getHouse(id) {
+        const house = await this.getResource(`/houses/${id}`);
+        return this._transformHouse(house);
+    }
+
+    async getAllBooks() {
+        const res = await this.getResource(`/books/`);
+        return res.map(this._transformBook);
+    }
+
+    async getBook(id) {
+        const book = await this.getResource(`/books/${id}`);
+        return this._transformBook(book);
+    }
+
+    _transformCharacter(char) {
+        return {
+            name: char.name,
+            gender: char.gender,
+            born: char.born,
+            died: char.died,
+            culture: char.culture
+        }
+    }
+
+    _transformHouse(house){
+        return {
+            name: house.name,
+            region: house.region,
+            words: house.words,
+            titles: house.titles,
+            overlord: house.overlord,
+            ancestralWeapons: house.ancestralWeapons
+        }
+    }
+
+    _transformBook(book) {
+        return {
+            name: book.name,
+            numberOfPages: book.numberOfPages,
+            publisher:book.publisher,
+            released: book.released
+        }
     }
 }
-
-const got = new GotService();
-
-got.getAllCharacters()
-    .then(res => {
-        res.forEach( item => console.log(item.name) );
-    });
-
-got.getCharacter(130)
-    .then(res => console.log(res));
