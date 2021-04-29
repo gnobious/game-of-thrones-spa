3b64759af44f91fe9e26bec85728ad2fc378abd3
Roman <rimer93@gmail.com>
2021-04-29 13:44:45 +0300

    Updates

---
+++ precache-manifest.92faf0a1ba292316123c1b436bb14bbd.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0810848fe4185b54fc6e4737fa4d6e64",
+    "url": "/game-of-thrones-spa/index.html"
+  },
+  {
+    "revision": "9e78c5cfbd80a75e15b3",
+    "url": "/game-of-thrones-spa/static/css/2.8aa5a7f8.chunk.css"
+  },
+  {
+    "revision": "4efbf319919cdc97d913",
+    "url": "/game-of-thrones-spa/static/css/main.fc9c2e7f.chunk.css"
+  },
+  {
+    "revision": "9e78c5cfbd80a75e15b3",
+    "url": "/game-of-thrones-spa/static/js/2.86da1dff.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/game-of-thrones-spa/static/js/2.86da1dff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4efbf319919cdc97d913",
+    "url": "/game-of-thrones-spa/static/js/main.5ac2e646.chunk.js"
+  },
+  {
+    "revision": "51342f2634c99ce4a193",
+    "url": "/game-of-thrones-spa/static/js/runtime-main.60a11a50.js"
+  },
+  {
+    "revision": "22389cf47ad284464c13506f2e22e8af",
+    "url": "/game-of-thrones-spa/static/media/error.22389cf4.jpg"
+  },
+  {
+    "revision": "6853c11381f570d27c1c130a43bfd9a7",
+    "url": "/game-of-thrones-spa/static/media/got.6853c113.jpg"
+  }
+]);
